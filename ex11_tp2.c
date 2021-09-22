23dc31c5b64af81be849f1bf6caff4c84f4e985d
hugostarte <47392672+hugostarte@users.noreply.github.com>
2021-09-22 14:58:09 +0200

    Update ex11_tp2.c

--- ex11_tp2.c
+++ ex11_tp2.c
@@ -3,15 +3,14 @@ Les huit erreurs de compilation ** */
 #include <stdio.h> 
 int main (void)
 {
-	int a=0, b=1, c;
-	printf ("Merci de saisir un premier nombre. \n"); 
-	scanf ("%d", &b);
-	printf ("Merci de saisir un deuxieme nombre. \n"); 
-	scanf ("%d", &c);
-	a = a + b + c;
-	if ( a > 1){
-		printf ("La somme des deux nombres lus est strictement positive. \n"); 
-	}
-	
-	return (0);
+  int a=0, b=1, c;
+  printf ("Merci de saisir un premier nombre. \n"); 
+  scanf ("%d", &b);
+  printf ("Merci de saisir un deuxieme nombre. \n"); 
+  scanf ("%d", &c);
+  a = b + c;
+  if ( a > 1){
+    printf ("La somme des deux nombres lus est strictement positive. \n"); 
+  }
+  return (0);
 }
